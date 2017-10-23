ae6a4cf66c8994ac60177f471f6ddb365bb14c5f
aaaa <aaaa@user83>
2017-10-23 20:04:28 +0900

    10.23

---
+++ src/main/java/com/newlecture/webapp/controller/Homecontroller.java
@@ -0,0 +1,16 @@
+package com.newlecture.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/*")
+public class Homecontroller {
+		@RequestMapping("index")
+		@ResponseBody
+		public String index() {
+			return "æ»≥Á«œººø‰";
+			//return "/WEB-INF/views/index.jsp";
+		}
+}
