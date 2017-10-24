b41ed1c06a457f1b8bd3637630421dae66cfdb21
aaaa <aaaa@user83>
2017-10-24 17:53:41 +0900

    script10.24

--- src/main/java/com/newlecture/webapp/controller/Homecontroller.java
+++ src/main/java/com/newlecture/webapp/controller/Homecontroller.java
@@ -1,16 +1,59 @@
 package com.newlecture.webapp.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/*")
 public class Homecontroller {
-		@RequestMapping("index")
-		@ResponseBody
-		public String index() {
-			return "안녕하세요";
-			//return "/WEB-INF/views/index.jsp";
+	
+	@RequestMapping("index")
+	public String index() {
+		// return "/WEB-INF/views/index.jsp";
+		return "home.index";
+	}
+	
+	@RequestMapping("upload")
+	@ResponseBody
+	public String upload(String title, MultipartFile file, HttpServletRequest request) throws IOException
+	{
+		System.out.println(title);
+		System.out.println(file.getOriginalFilename());
+		
+		ServletContext ctx = request.getServletContext();	//request의 공유자원을 ctx에 넣음
+		String path = ctx.getRealPath("/resource/upload");	//"resource/upload"의 실제 폴더가 어디에 있는지 request의 공유자원중 실제경로를 가져옴.
+		File f = new File(path);							//경로는 파일을 등록하기 위한 준비사항- 폴더생성
+		
+		if(!f.exists()) {//폴더생성
+			if(!f.mkdirs())
+				System.out.println("디렉토리를 생성할 수가 없습니다.");
 		}
+		
+		path += File.separator + file.getOriginalFilename();//실제파일을 저장하기 위한 파일객체- File.separator:운영체제마다 '/'or '\' 다름. 자동으로 맞게 설정 
+		File f2 = new File(path);							
+		System.out.println("path : "+path);
+		
+		InputStream fis = file.getInputStream(); //읽기			
+		OutputStream fos = new FileOutputStream(f2);//쓰기
+		
+		byte[] buf = new byte[1024];
+		int size = 0;
+		while((size=fis.read(buf))>0)
+			fos.write(buf, 0, size);
+		fos.close();
+		fis.close();	
+		return title;
+	}
+
 }
---
+++ src/main/webapp/resource/js/script.jsp
@@ -0,0 +1,59 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Script</title>
+<script type="text/javascript">
+/* window.addEventListener("load", function(e) {
+	var fileInput = document.querySelector("#ex3-upload input");
+	fileInput.onchange = function(){
+		var file = fileInput.files[0];
+		alert(file.name);
+		var formData = new FormData();
+	 	formData.append("title","테스트");
+	 	formData.append("file",file);
+	 	var xhr = new XMLHttpRequest();
+	 	xhr.onerror = function(e) {
+	 		alert("예기치 못한 오류가 발생하였습니다.");   
+	    };
+	    xhr.open("POST","../../upload?${_csrf.parameterName}=${_csrf.token}", true); // 데이터를 요청하기 위한 설정 // 세번째 파라메터 -> 비동기(true) or 동기(false) 설정
+	    xhr.send(formData); // 요청
+	};
+	
+}); */
+</script>
+</head>
+<body>
+<!-- Ajax로 파일 전송하기와 트리거 그리고 파일목록뷰어 -->
+   <div id="ex3-upload">
+   	<hr>
+      <form action="../../upload" method="post" enctype="multipart/form-data">
+         <div>
+            <input type="submit" value="전송" />
+         </div>
+         <div id="clone-container">
+            <table border="1">
+               <tbody>
+                  <tr>
+                     <td>제목</td>
+                     <td><input type="text" name="title" /></td>
+                  </tr>
+                  <tr>
+                     <td>파일</td>
+                     <td><input type="file" name="file"/></td>
+                  </tr>
+               </tbody>
+            </table>
+         </div>
+      </form>
+      <div>
+      	<ul>
+      		<li></li>
+      	</ul>
+      </div>
+   </div>
+   <hr />
+</body>
+</html>
